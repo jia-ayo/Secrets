650f3eb6cee41e525064b71d19e17a7553a8e1fd
jia8769 <76929813+jia8769@users.noreply.github.com>
2022-09-17 12:04:01 +0100

    add bcrypt

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose =require("mongoose");
-const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 11;
+
 
 const app = express();
 
@@ -26,48 +28,47 @@ app.get('/', (req, res) => {
     res.render('home');
 });
 
+app.get('/register', (req, res) => {
+    res.render('register');
+});
+app.post("/register", (req, res)=>{
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save((err)=>{
+            if (err) {
+                console.log(err)
+            } else {
+                res.render("secrets")
+            }
+        })
+    });
+
+})
+
 app.get('/login', (req, res) => {
     res.render('login');
 });
 app.post("/login" , (req, res)=>{
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email:username}, (err, foundUser)=>{
         if(err){
             console.log(err)
         }else{
             if(foundUser){
-                if (foundUser.password === password){
-                    res.render("secrets")
-                }
+                bcrypt.compare(password, foundUser.password,  (err, result) => {
+                    if (result == true){ 
+                       res.render("secrets")
+                    }
+                });
             }
         }
     })
 })
-
-app.get('/register', (req, res) => {
-    res.render('register');
-});
-app.post("/register", (req, res)=>{
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    })
-    newUser.save((err)=>{
-        if (err) {
-            console.log(err)
-        } else {
-            res.render("secrets")
-        }
-    })
-})
-
-
-
-
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
