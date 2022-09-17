095ac649739999c253711ec0bdc85d0f0489f283
jia8769 <76929813+jia8769@users.noreply.github.com>
2022-09-17 01:07:34 +0100

    added encryption

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose =require("mongoose");
+const encrypt = require("mongoose-encryption");
 
 const app = express();
 
@@ -13,10 +14,13 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 mongoose.connect("mongodb://localhost/userDB");
 
-const userSchema ={
+const userSchema = new mongoose.Schema({
     email: String,
     password: String
-}
+});
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt,{ secret: secret, encryptedFields: ["password"] });
+
 
 const User = new mongoose.model("User", userSchema)
 
