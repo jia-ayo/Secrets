7b360803511b5ac58e543a4862d18556764d035b
jia8769 <76929813+jia8769@users.noreply.github.com>
2022-09-25 07:44:51 +0100

    finished google oauth

--- app.js
+++ app.js
@@ -27,21 +27,30 @@ app.use(passport.initialize());
 app.use(passport.session());
  
 mongoose.connect("mongodb://localhost/userDB");
+// mongoose.set("useCreateIndex", true); 
 
 const userSchema = new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
 userSchema.plugin(findOrCreate);
   
-const User = new mongoose.model("User", userSchema)
+const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
 
+passport.serializeUser(function(user, done) {
+    done(null, user._id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -49,6 +58,7 @@ passport.use(new GoogleStrategy({
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
   },
   function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
     User.findOrCreate({ googleId: profile.id }, function (err, user) {
       return cb(err, user);
     });
@@ -59,13 +69,20 @@ app.get("/", (req, res) => {
     res.render('home');
 });
 
-app.get('/register', (req, res) => {
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile"] })
+);
+app.get("/auth/google/secrets", 
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
+
+app.get("/register", (req, res) => {
     res.render('register');
 });
-
-app.get("/auth/google", (req,res)=>{
-    passport.authenticate("google",{ scope: ["profile"] })
-})
 
 app.get("/secrets", (req,res) => {
     if(req.isAuthenticated()){
